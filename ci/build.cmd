e4ee241d79d757c2549d7e3a2dc8db3877862399
Cenlun Chung Po Lun <104301294+ccpl17@users.noreply.github.com>
2024-12-01 14:47:48 +0800

    ⬆️ build: bump app version ⬆️ 24.12.0

--- ci/build.cmd
+++ ci/build.cmd
@@ -12,7 +12,7 @@ set title=藍眼引擎建置器 Aoi Hitomi Engine Builder
 set app_name=aoi-hitomi-engine
 
 @rem 應用程式版本
-set app_version=24.11.0
+set app_version=24.12.0
 
 @rem Node.js 版本
 set node_version=22.11.0
