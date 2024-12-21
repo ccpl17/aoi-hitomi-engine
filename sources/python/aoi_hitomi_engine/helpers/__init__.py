2062ef2011b651cdaa25e89de957f6c27ff231aa
Cenlun Chung Po Lun <104301294+ccpl17@users.noreply.github.com>
2024-12-21 20:20:42 +0800

    ✨ feat(py/helper): logging helper

--- sources/python/aoi_hitomi_engine/helpers/__init__.py
+++ sources/python/aoi_hitomi_engine/helpers/__init__.py
@@ -1,2 +1,3 @@
 from aoi_hitomi_engine.helpers._file_picker_helper import FilePickerHelper
+from aoi_hitomi_engine.helpers._logging_helper import RichLogger
 from aoi_hitomi_engine.helpers._path_helper import PathHelper
