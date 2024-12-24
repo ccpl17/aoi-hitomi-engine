dfa17d336a31c59e71cdefbc70954ca892dce8dd
Cenlun Chung Po Lun <104301294+ccpl17@users.noreply.github.com>
2024-12-24 23:17:36 +0800

    ✨ feat(py/helper/path): update `app_data_dir` for macOS support

--- sources/python-app-core/aoi_hitomi_engine/helpers/_path_helper.py
+++ sources/python-app-core/aoi_hitomi_engine/helpers/_path_helper.py
@@ -1,5 +1,6 @@
 from os import getenv
 from pathlib import Path
+from platform import system
 from typing import final
 
 
@@ -21,7 +22,10 @@ class PathHelper:
 
     @staticmethod
     def app_data_dir() -> Path:
-        return Path(getenv("APPDATA")) / "aoi-hitomi-engine"
+        if system() == "Windows":
+            return Path(getenv("APPDATA")) / "aoi-hitomi-engine"
+        elif system() == "Darwin":
+            return Path(Path.home() / "Library" / "Application Support" / "aoi-hitomi-engine")
 
     @classmethod
     def app_log_dir(cls) -> Path:
